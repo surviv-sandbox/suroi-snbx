1c0b261f92fb0a7040c7e9fb8b9a821fc1ad1c87
surviv-sandbox <106218385+surviv-sandbox@users.noreply.github.com>
2022-07-09 22:03:11 -0400

    Delete loadLevels.js

--- loadLevels.js
+++
@@ -1,5 +0,0 @@
-import { level as level0 } from "./assets/levels/level0/level.js";
-import { level as level1 } from "./assets/levels/level1/level.js";
-import { level as level2 } from "./assets/levels/level2/level.js";
-gamespace.levels = [level0, level1, level2];
-gamespace.events.dispatchEvent("levelsLoaded");
