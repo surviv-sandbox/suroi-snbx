cc082228e8f421cc1c81f7026b702a38b75ae6a0
surviv-sandbox <106218385+surviv-sandbox@users.noreply.github.com>
2022-07-09 21:49:28 -0400

    Delete guides directory

--- guides/internalLinks.js
+++
@@ -1,6 +0,0 @@
-(() => {
-    document.querySelectorAll("h3").forEach((e, i) => {
-        e.id = `h3-${i}`;
-        e.innerHTML = `<a href="./#h3-${i}" class="no-style" style="color: inherit; text-decoration: none">${e.innerHTML}</a>`;
-    });
-})();
