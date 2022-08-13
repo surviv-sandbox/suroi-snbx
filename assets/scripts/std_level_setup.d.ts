505e37356397dafc4c3a71bffc37ee9b3a8d7a53
surviv-sandbox <106218385+surviv-sandbox@users.noreply.github.com>
2022-08-13 16:25:44 -0400

    Delete std_level_setup.d.ts

--- assets/scripts/std_level_setup.d.ts
+++
@@ -1,5 +0,0 @@
-declare function std_setup(engine: Matter.Engine, world: Matter.World, p5: import("p5"), level: typeof import("../levels/level0/level").level, font?: {
-    font: string | import("p5").Font;
-    size?: number;
-}): void;
-export default std_setup;
