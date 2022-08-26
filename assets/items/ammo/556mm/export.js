735cc9be4398669f62360dd757f52f84da4b017e
surviv-sandbox <106218385+surviv-sandbox@users.noreply.github.com>
2022-08-26 16:40:25 -0400

    0.8.3

---
+++ assets/items/ammo/556mm/export.js
@@ -0,0 +1,33 @@
+// @ts-check
+/**
+ * @type {ammoData}
+ */
+export default {
+    name: "5.56x45mm",
+    targetVersion: "0.8.3",
+    tints: {
+        normal: "#A9FF92",
+        saturated: () => gamespace.settings.bonusFeatures.useInterpolatedSaturatedTracers ? "#7EFF5C" : "#A9FF92",
+        chambered: "#36FF00"
+    },
+    alpha: {
+        rate: 0.92,
+        min: 0.14,
+        max: 1
+    },
+    spawnVar: 0,
+    imageOffset: {
+        parr: 0,
+        perp: 0
+    },
+    projectileInfo: {
+        type: "bullet",
+        img: "../shared/tracer.png"
+    },
+    casing: {
+        img: "./shell-556mm.png",
+        lifetime: () => +meanDevPM_random(750, 250, false),
+        width: 6762 / 437,
+        height: 46
+    }
+};
